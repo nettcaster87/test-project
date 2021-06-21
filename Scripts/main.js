8c3518c7a08c86eb6eac3366b70f3dda35f8ca38
Khigha87 <nettcaster87@gmail.com>
2021-06-21 13:22:20 +0200

    Add getUserName button js

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,10 +1,35 @@
 let myImage = document.querySelector('img');
 
-myImage.onclick = function(){
-	let	mySrc = myImage.getAttribute('src');
+myImage.onclick = function() {
+	let mySrc = myImage.getAttribute('src');
 	if(mySrc === 'Images/git_status_sequence.png') {
 		myImage.setAttribute('src','Images/bmw-m2.jpg');
 	} else {
 		myImage.setAttribute('src','Images/git_status_sequence.png');
-	}
+	} 
+} 
+/* 
+This is how you comment
+*/
+
+//This too
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+	let myName = prompt('Please enter your name.');
+	localStorage.setItem('name', myName);
+	myHeading.textContent = 'Mozilla is cool, ' + myName;
 }
+
+if (!localStorage.getItem('name')) {
+	setUserName();
+} else {
+	let storedName = localStorage.getItem('name');
+	myHeading.textContent = 'Mozilla is cool, ' + storedName;
+}
+
+myButton.onclick = function() {
+	setUserName();
+}	
